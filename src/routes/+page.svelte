50f5b815b731776c0ae0ed01f63858703ae61d20
Valantina Tsoi <99823443+valantinatsoi@users.noreply.github.com>
2022-09-24 23:56:24 +0800

    new repo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let title = "Hello"
+
+  const updateTitle = () => {
+    title = "Goodbye"
+  }
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <button on:click={updateTitle}>Change Title</button>
+  <input type="text" bind:value={title}>
+</div>
+
+<div class="index">
+  <a href="/about">About</a>
+  <a href="/guides">Guides</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
